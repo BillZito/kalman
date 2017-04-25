759215a1c96ee2509dd075d755ab3e900f5a415e
Bill Zito <bill.r.zito@gmail.com>
2017-04-25 13:32:52 -0700

    Complete predict and update for lasers

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -43,6 +43,8 @@ FusionEKF::FusionEKF() {
   H_laser_ << 1, 0, 0, 0,
               0, 1, 0, 0;
 
+  noise_ax_ = 9;
+  noise_ay_ = 9;
   // Hj_ << 0,
   //        0,
   //        0;
@@ -139,17 +141,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   ekf_.F_(1, 3) = dt;
   // cout << "f set" << endl;
 
-  int noise_ax = 9;
-  int noise_ay = 9;
-
   double dt_2 = dt * dt;
   double dt_3 = dt_2 * dt;
   double dt_4 = dt_3 * dt;
 
-  ekf_.Q_ << dt_4 * noise_ax / 4, 0, dt_3 * noise_ax / 2, 0,
-             0, dt_4 * noise_ay / 4, 0, dt_3 * noise_ay / 2,
-             dt_3 * noise_ax / 2, 0, dt_2 * noise_ax, 0,
-             0, dt_3 * noise_ay / 2, 0, dt_2 * noise_ay;  
+  ekf_.Q_ << dt_4 * noise_ax_ / 4, 0, dt_3 * noise_ax_ / 2, 0,
+             0, dt_4 * noise_ay_ / 4, 0, dt_3 * noise_ay_ / 2,
+             dt_3 * noise_ax_ / 2, 0, dt_2 * noise_ax_, 0,
+             0, dt_3 * noise_ay_ / 2, 0, dt_2 * noise_ay_;  
   // cout << "q set " << endl;
 
   ekf_.Predict();
@@ -166,9 +165,16 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    // ekf_.H_ << H_radar_;
+    // ekf_.R_ << R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
-
+    cout << "before update" << endl;
+    ekf_.H_ << H_laser_;
+    ekf_.R_ << R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
+    cout << "after update" << endl;
   }
 
   // print the output
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,5 @@
 #include "kalman_filter.h"
+#include <iostream> 
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -7,6 +8,7 @@ KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
 
+//**need to init!!
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
   x_ = x_in;
@@ -22,13 +24,40 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  std::cout << "before predict" << std::endl;
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
+  std::cout << "after predict" << std::endl;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Kalman Filter equations
+      calc y, using Hlaser to move into sensor space
+      calc S based on variance and laser diff
+      calc K based on S and R (measurement err)
+      calc x based on K
+      calc P based on S and R
   */
+  VectorXd y = VectorXd(2,1);
+  y = z - H_ * x_;
+  std::cout << "y is" << y << std::endl;
+
+  MatrixXd S = MatrixXd(2, 2);
+  S = H_ * P_ * H_.transpose() + R_;
+  std::cout << "S is" << S << std::endl;
+
+  MatrixXd K = MatrixXd(2, 2);
+  K = P_ * H_.transpose() * S.inverse();
+
+  x_ = x_ + K * y;
+
+  long x_size = x_.size();
+  std::cout << "xsize" << x_size << std::endl;
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K*H_) * P_;
+  std::cout << "pred complete" << std::endl;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
